4f5ed336317067e102e07da95c47c5ee3746c1f4
hugo@122 <hugo@scorenco.com>
2022-05-05 08:41:53 +0200

    Administration

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Product;
-use App\Entity\Content;
+
 use App\Entity\ProductType;
 use App\Entity\TypeCategory;
 use Doctrine\ORM\EntityManagerInterface;
@@ -28,10 +28,11 @@ class AdminController extends AbstractController
      */
     public function index(): Response
     {
+        $products = $this->getDoctrine()->getRepository(Product::class)->findAll();
 
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
-
+            'products' => $products,
         ]);
     }
 }
